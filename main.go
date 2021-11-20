9e5e84fd52d6472ca7113c5f45b031d03c8d6bf3
Rhiadc <rhiad.ciccoli@gmail.com>
2021-11-20 18:46:20 -0300

    add flag option

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,22 +11,34 @@ import (
 )
 
 func main() {
-	in, err := ioutil.ReadFile("exemplo.txt")
+
+	wordPtr := flag.String("test", "exemplo.txt", "Please specify the path of the test you want to take")
+
+	in, err := ioutil.ReadFile(*wordPtr)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	r := csv.NewReader(strings.NewReader(string(in)))
+	correct, total := 0, 0
 
 	for {
+		var response string
 		record, err := r.Read()
 		if err == io.EOF {
+			fmt.Println("Correct answers: ", correct, "/", total)
 			break
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(record[0], record[1])
+
+		fmt.Printf("%v\nResponse: ", record[0])
+		fmt.Scanln(&response)
+		if response == record[1] {
+			correct += 1
+		}
+		total += 1
 	}
 
 }
